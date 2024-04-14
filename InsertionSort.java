e31ea4350f9f3f6b8e8e338e04f8d52b1f33c4c4
Meet <meet16123@gmail.com>
2024-04-14 17:34:23 -0400

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {5,2,7,1};
+        insertionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i-1;
+            while (j >= 0 && array[j] > temp) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+    }
+}
