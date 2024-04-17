ce0787e90cedc20a5e926b9b5c61d3ffc184b173
Meet Mangroliya <121549250+Meet16123@users.noreply.github.com>
2024-04-16 21:52:50 -0400

    Update QueueB.java

--- ArraryBasedQueue.java
+++ ArraryBasedQueue.java
@@ -1,39 +1,68 @@
-class Queue {
-    static  int front, rear , capacity;
-    static  int queue[];
+public class QueueB {
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear;
 
-    Queue(int c) {
-        front = rear = 0;
-        capacity = c;
-        queue = new int[capacity];
-    }
 
-    static void enque(int data) {
-            if(capacity == rear) {
-                System.out.println("Full");
-                return;
-            } else {
-                queue[rear] = data;
-                rear++;
-            }
-            return;
-    }
-
-    static void deque() {
-        if(front == rear) {
-            System.out.println("Empty");
-            return;
-        } else {
-
+        Queue(int size) {
+            this.size = size;
+            arr = new int[size];
+            rear = -1;
         }
 
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+
+        public static boolean isFull() {
+            return rear == size-1;
+        }
+
+
+        public static void add(int data) {
+            if(isFull()) {
+                System.out.println("Overflow");
+                return;
+            }
+
+
+            arr[++rear] = data;
+        }
+
+
+        //O(n)
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i=0; i<rear; i++) {
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return arr[0];
+        }
     }
-
-
-}
-
-public class ArraryBasedQueue {
-    public static void main(String[] args) {
-
+    public static void main(String args[]) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+        System.out.println(q.peek());
     }
 }
